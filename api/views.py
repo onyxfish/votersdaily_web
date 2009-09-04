1a161fd17a29ebf8660ee3ade08583ede543ff73
Christopher Groskopf <staringmonkey@gmail.com>
2009-09-04 06:54:30 -0700

    Added API support for query options.

--- api/views.py
+++ api/views.py
@@ -10,89 +10,97 @@ from votersdaily_web import settings
 def couchdb_urlopen(server, database, design, view, **options):
     """
     Build a CouchDB view url, open it, and return the raw results.
-    
-    TODO: handle options
     """
-    url = '%s/%s/_design/%s/_view/%s' % (server, database, design, view)
+    query = urllib.urlencode(options)
+    url = '%s/%s/_design/%s/_view/%s?%s' % (
+        server, database, design, view, query)
     
     return urllib2.urlopen(url)
+
+def get_query_options(request):
+    """
+    Parses query options common to all API views from a request's GET dict.
+    """
+    options = {}
+    
+    try:
+        options['endkey'] = request.GET['start']
+    except KeyError:
+        pass
+    
+    try:
+        options['startkey'] = request.GET['end']
+    except KeyError:
+        pass
+    
+    options['descending'] = 'true'
+    options['inclusive_start'] = 'true'
+    options['inclusive_end'] = 'false'
+    
+    return options
 
 def events_all(request):
     """
     Return all events in the database.
     """
+    if request.method != 'GET':
+        raise Http404
     
-    # JSON
-    #if request.accepts('application/json'):
-        # GET
-    if request.method == 'GET':
-        #try:
-        json_result = couchdb_urlopen(
-            settings.COUCHDB_SERVER_URI, 
-            settings.COUCHDB_EVENTDB_NAME, 
-            'api', 
-            'all')
-        #except HttpError:
-            #TODO
+    options = get_query_options(request)
+    
+    # TODO - support alternate formats
+    format = request.GET.get('format', 'json')
+    
+    json_result = couchdb_urlopen(
+        settings.COUCHDB_SERVER_URI, 
+        settings.COUCHDB_EVENTDB_NAME, 
+        'api', 
+        'all',
+        **options)
             
-        return HttpResponse(json_result, mimetype='application/json')
-    else:
-        raise NotImplementedError()
-    #else:
-    #    raise NotImplementedError()
+    return HttpResponse(json_result, mimetype='application/json')
 
 def events_branch(request, branch):
     """
     Return the events for a specific branch of government, using a pre-
     generated view.
     """
+    if request.method != 'GET':
+        raise Http404
     
-    # JSON
-    #if request.accepts('application/json'):
-        # GET
-    if request.method == 'GET':        
-        #try:
-        json_result = couchdb_urlopen(
-            settings.COUCHDB_SERVER_URI, 
-            settings.COUCHDB_EVENTDB_NAME, 
-            'api', 
-            urllib.quote(branch))
-        #except HttpError:
-            #TODO
+    options = get_query_options(request)
+    
+    # TODO - support alternate formats
+    format = request.GET.get('format', 'json')
+    
+    json_result = couchdb_urlopen(
+        settings.COUCHDB_SERVER_URI, 
+        settings.COUCHDB_EVENTDB_NAME, 
+        'api', 
+        urllib.quote(branch),
+        **options)
 
-        return HttpResponse(json_result, mimetype='application/json')
-    else:
-        raise NotImplementedError()
-    #else:
-    #    raise NotImplementedError()
+    return HttpResponse(json_result, mimetype='application/json')
 
 def events_entity(request, entity):
     """
     Return the events for a specific government entity, using a pre-generated 
     view.
     """
+    if request.method != 'GET':
+        raise Http404
     
-    # JSON
-    #if request.accepts('application/json'):
-        # GET
-    if request.method == 'GET':
-        #try:        
-        json_result = couchdb_urlopen(
-            settings.COUCHDB_SERVER_URI, 
-            settings.COUCHDB_EVENTDB_NAME, 
-            'api',
-            urllib.quote(entity))
-        
-            #'/_design/api/_view/executive?startkey="2009-08"&descending=true'))
-        #except HttpError:
-            #TODO
-            #
+    options = get_query_options(request)
+    
+    # TODO - support alternate formats
+    format = request.GET.get('format', 'json')
 
-#
+    json_result = couchdb_urlopen(
+        settings.COUCHDB_SERVER_URI, 
+        settings.COUCHDB_EVENTDB_NAME, 
+        'api',
+        urllib.quote(entity),
+        **options)
 
-        return HttpResponse(json_result, mimetype='application/json')
-    else:
-        raise NotImplementedError()
-    #else:
-    #    raise NotImplementedError()
+    return HttpResponse(json_result, mimetype='application/json')
     
