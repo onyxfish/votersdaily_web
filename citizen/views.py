6c88351a7e4354ff10be575e281910e6104fa946
Christopher Groskopf <staringmonkey@gmail.com>
2009-09-04 23:11:48 -0700

    Cleaned up Citizen app views.
    Added entity Django view.
    Extracted GET param handling into its own method.

--- citizen/views.py
+++ citizen/views.py
@@ -1,4 +1,5 @@
 import json
+import urllib
 import urllib2
 
 from django.core.urlresolvers import reverse
@@ -8,69 +9,83 @@ from django.shortcuts import render_to_response
 from votersdaily_web import settings
 
 
+def get_query_options(request):
+    """
+    Parses query options common to all Citizen views from a request's GET dict.
+    """
+    options = {}
+    
+    try:
+        options['start'] = request.GET['start']
+    except KeyError:
+        pass
+    
+    try:
+        options['end'] = request.GET['end']
+    except KeyError:
+        pass
+    
+    options['format'] = 'json'
+    
+    return options
+
 def all(request):
     """
-    Renders a list of all documents.
+    Renders a list of all events.
     """
     if request.method != 'GET':
         raise Http404
     
-    options = {}
+    api_url = settings.API_ROOT + reverse('api_events_all')
     
-    format = request.GET.get('format', 'json')
+    options = get_query_options(request)
+    query_string = urllib.urlencode(options)
     
-    try:
-        options['endkey'] = request.GET['start']
-    except KeyError:
-        pass
+    if query_string:
+        api_url = '%s?%s' % (api_url, query_string)
     
-    try:
-        options['startkey'] = request.GET['startkey']
-    except KeyError:
-        pass
-    
-    options['descending'] = 'true'
-    
-    query_string = ''
-    
-    url = settings.API_ROOT + reverse('api_events_all') + query_string
-    
-    api_result = urllib2.urlopen(url)
+    api_result = urllib2.urlopen(api_url)
     data = json.loads(api_result.read())
             
     return render_to_response('all.html', { 'documents': data['rows'] })
 
 def branch(request, branch):
     """
-    Renders a list of all documents.
+    Renders a list of all events for a specific branch.
     """
-    print branch
-    
     if request.method != 'GET':
         raise Http404
     
-    options = {}
+    api_url = settings.API_ROOT + reverse('api_events_branch', kwargs={'branch':branch})
     
-    format = request.GET.get('format', 'json')
+    options = get_query_options(request)
+    query_string = urllib.urlencode(options)
     
-    try:
-        options['endkey'] = request.GET['start']
-    except KeyError:
-        pass
+    if query_string:
+        api_url = '%s?%s' % (api_url, query_string)
     
-    try:
-        options['startkey'] = request.GET['startkey']
-    except KeyError:
-        pass
-    
-    options['descending'] = 'true'
-    
-    query_string = ''
-    
-    url = settings.API_ROOT + reverse('api_events_branch', kwargs={'branch':branch}) + query_string
-    
-    api_result = urllib2.urlopen(url)
+    api_result = urllib2.urlopen(api_url)
     data = json.loads(api_result.read())
                 
     return render_to_response('branch.html', { 'documents': data['rows'] })
+
+def entity(request, entity):
+    """
+    Renders a list of all events for a specific entity.
+    """
+    if request.method != 'GET':
+        raise Http404
+    
+    api_url = settings.API_ROOT + reverse('api_events_entity', kwargs={'entity':entity})
+    
+    options = get_query_options(request)
+    query_string = urllib.urlencode(options)
+    
+    if query_string:
+        api_url = '%s?%s' % (api_url, query_string)
+    
+    api_result = urllib2.urlopen(api_url)
+    data = json.loads(api_result.read())
+                
+    return render_to_response('entity.html', { 'documents': data['rows'] })
     
--- urls.py
+++ urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     (r'^api/', include('votersdaily_web.api.urls')),
     
     # Citizen
-    url(r'^all', 'votersdaily_web.citizen.views.all', name='all'),
-    url(r'^branch/(?P<branch>.*)', 'votersdaily_web.citizen.views.branch', name='branch'),
+    url(r'^events/all', 'votersdaily_web.citizen.views.all', name='events_all'),
+    url(r'^events/branch/(?P<branch>.*)', 'votersdaily_web.citizen.views.branch', name='events_branch'),
+    url(r'^events/entity/(?P<entity>.*)', 'votersdaily_web.citizen.views.entity', name='events_entity'),
 )
