8f206c93410e4b801487ae18350ef2c578c72ad6
Christopher Groskopf <staringmonkey@gmail.com>
2009-09-06 12:25:46 -0700

    Added support for iCal format.

--- api/views.py
+++ api/views.py
@@ -7,13 +7,20 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 
 from votersdaily_web import settings
 
-def couchdb_urlopen(server, database, design, view, **options):
+def couchdb_urlopen(server, database, design, view, format, **options):
     """
     Build a CouchDB view url, open it, and return the raw results.
     """
     query = urllib.urlencode(options)
-    url = '%s/%s/_design/%s/_view/%s?%s' % (
-        server, database, design, view, query)
+    
+    if format == 'json':
+        url = '%s/%s/_design/%s/_view/%s?%s' % (
+            server, database, design, view, query)
+    elif format == 'ical':
+        url = '%s/%s/_design/%s/_list/ical/%s?%s' % (
+            server, database, design, view, query)
+    else:
+        raise Http404
     
     return urllib2.urlopen(url)
 
@@ -39,6 +46,21 @@ def get_query_options(request):
     
     return options
 
+def get_result_format_and_mimetype(request):
+    """
+    Determine the format and mimetype from the URL GET parameters.
+    """
+    format = request.GET.get('format', 'json')
+    
+    if format == 'json':
+        mimetype = 'application/json'
+    elif format == 'ical':
+        mimetype = 'text/calendar'
+    else:
+        raise Http404()
+    
+    return (format, mimetype)
+
 def events_all(request):
     """
     Return all events in the database.
@@ -46,19 +68,18 @@ def events_all(request):
     if request.method != 'GET':
         raise Http404
     
-    options = get_query_options(request)
+    options = get_query_options(request)    
+    format, mimetype = get_result_format_and_mimetype(request)
     
-    # TODO - support alternate formats
-    format = request.GET.get('format', 'json')
-    
-    json_result = couchdb_urlopen(
+    result = couchdb_urlopen(
         settings.COUCHDB_SERVER_URI, 
         settings.COUCHDB_EVENTDB_NAME, 
         'api', 
         'all',
+        format,
         **options)
-            
-    return HttpResponse(json_result, mimetype='application/json')
+    
+    return HttpResponse(result, mimetype)
 
 def events_branch(request, branch):
     """
@@ -68,19 +89,18 @@ def events_branch(request, branch):
     if request.method != 'GET':
         raise Http404
     
-    options = get_query_options(request)
+    options = get_query_options(request)    
+    format, mimetype = get_result_format_and_mimetype(request)
     
-    # TODO - support alternate formats
-    format = request.GET.get('format', 'json')
-    
-    json_result = couchdb_urlopen(
+    result = couchdb_urlopen(
         settings.COUCHDB_SERVER_URI, 
         settings.COUCHDB_EVENTDB_NAME, 
         'api', 
         urllib.quote(branch),
+        format,
         **options)
 
-    return HttpResponse(json_result, mimetype='application/json')
+    return HttpResponse(result, mimetype)
 
 def events_entity(request, entity):
     """
@@ -90,17 +110,16 @@ def events_entity(request, entity):
     if request.method != 'GET':
         raise Http404
     
-    options = get_query_options(request)
-    
-    # TODO - support alternate formats
-    format = request.GET.get('format', 'json')
+    options = get_query_options(request)    
+    format, mimetype = get_result_format_and_mimetype(request)
 
-    json_result = couchdb_urlopen(
+    result = couchdb_urlopen(
         settings.COUCHDB_SERVER_URI, 
         settings.COUCHDB_EVENTDB_NAME, 
         'api',
         urllib.quote(entity),
+        format,
         **options)
 
-    return HttpResponse(json_result, mimetype='application/json')
+    return HttpResponse(result, mimetype)
     
